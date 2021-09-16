750b698e73d2cb17b09a592dac4b107dbe358035
samhi113 <samhiggins113@gmail.com>
2021-09-16 12:14:45 -0400

    Finished SalesCommission

--- src/SalesCommission.java
+++ src/SalesCommission.java
@@ -1,3 +1,4 @@
+
 /*
 A car salesperson typically earns an hourly wage and a commission on any sales they completed. They typically work 30 to 40
 hours a week, earning $5.25 to $7.50 an hour. Additionally, they earn 1% to 3% of all sales they've completed that week,
@@ -18,10 +19,50 @@ Total Earnings: $1184.38
 
  */
 
+/*
+Step 1: Get inputs - Hourly rate, hours turned, commission %, and $ of sales made
+Step 2: Calculate hourly pay
+Step 3: Calculate pay from commission
+Step 4: Combine pays together
+Step 5: Output results
+ */
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class SalesCommission {
-
     public static void main(String[] args) {
+        double hourlyRate= Double.parseDouble(JOptionPane.showInputDialog("How much do you make each hour?"));
+        double hoursTurned= Double.parseDouble(JOptionPane.showInputDialog("How many hours did you turn this week?"));
+        double commissionRate= (Double.parseDouble(JOptionPane.showInputDialog("What % do you make off of sales?")))/100.00;
+        double salesMade= Double.parseDouble(JOptionPane.showInputDialog("How much sales did you make this week?"));
 
+        double hourlyPay = calcHourlyPay(hourlyRate, hoursTurned);
+
+        double commissionPay = calcCommissionPay(commissionRate, salesMade);
+
+        double totalPay = calcTotalPay(hourlyPay, commissionPay);
+
+        displayResults(totalPay);
+
+        System.exit((int)(totalPay*100));
     }
 
+    public static double calcHourlyPay(double hourlyRate, double hoursTurned){
+        return hourlyRate * hoursTurned;
+    }
+
+    public static double calcCommissionPay(double commissionRate, double salesMade){
+        return commissionRate * salesMade;
+    }
+
+    public static double calcTotalPay(double hourlyPay, double commissionPay){
+        return hourlyPay + commissionPay;
+    }
+
+    public static void displayResults(double totalPay){
+        DecimalFormat money = new DecimalFormat("$#,###.00");
+
+        JOptionPane.showMessageDialog(null, "This week, you made " + money.format(totalPay) + "\n" + "At this rate, you'll make " + money.format(totalPay*(52.142857)) + " this year.");
+    }
 }
